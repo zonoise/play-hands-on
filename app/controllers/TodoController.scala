e7c58b4ce396837d408a2adb6c5eee924f333707
zonoise <zonoise@gmail.com>
2017-08-01 01:27:45 +0900

    add evolutions

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -1,5 +1,5 @@
 package controllers
-import services.Todo
+import services.{Todo, TodoService}
 //コピペ
 import javax.inject._
 import play.api.mvc._
@@ -9,14 +9,13 @@ import play.api.data.Forms._
 /**
   * Created by user on 2017/08/01.
   */
-class TodoController @Inject()(mcc: MessagesControllerComponents ) extends MessagesAbstractController(mcc){
+class TodoController @Inject()(todoService: TodoService, mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
   def helloworld() = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok("Hello World")
   }
 
   def list() = Action { implicit request: MessagesRequest[AnyContent] =>
-    val items: Seq[Todo] = Seq(Todo("Todo1"), Todo("Todo2"))
-
+    val items: Seq[Todo] = todoService.list()
     Ok(views.html.list(items))
   }
 }
