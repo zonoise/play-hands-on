fcdd882ec6e00fca8680895dee0e4d38d56a4bb2
zonoise <zonoise@gmail.com>
2017-08-01 01:04:26 +0900

    hello world

---
+++ app/controllers/TodoController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+//コピペ
+import javax.inject._
+import play.api.mvc._
+
+import play.api.data._
+import play.api.data.Forms._
+/**
+  * Created by user on 2017/08/01.
+  */
+class TodoController @Inject()(mcc: MessagesControllerComponents ) extends MessagesAbstractController(mcc){
+  def helloworld() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok("Hello World")
+  }
+}
