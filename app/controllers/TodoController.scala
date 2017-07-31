6facefdf9876d30d737bfbb0f5e49562cc580e73
zonoise <zonoise@gmail.com>
2017-08-01 01:10:06 +0900

    7. add list template

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -13,4 +13,9 @@ class TodoController @Inject()(mcc: MessagesControllerComponents ) extends Messa
   def helloworld() = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok("Hello World")
   }
+
+  def list() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val message: String = "ここにリストを表示"
+    Ok(views.html.list(message))
+  }
 }
